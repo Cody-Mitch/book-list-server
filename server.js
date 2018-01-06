d6b7de8b6f8a1e41683cc7a340b970adacda10c9
cody <codyjeromegreen@gmail.com>
2018-01-06 15:52:30 -0800

    set up heroku

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const PORT = process.env.PORT || 3000
+const app = express()
+
+const __API_URL__ = https://cg-mh-booklist.herokuapp.com
+
+app.get('/test', (req, res) => res.send('hello world'))
+
+app.listen(PORT, () => console.log('listening on port ', PORT))
