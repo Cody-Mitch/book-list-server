f34e6059142419f29439b4eb2972a02a739fd6f2
cody <codyjeromegreen@gmail.com>
2018-01-06 18:22:26 -0800

    working on getting books into our db

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+'use strict';
 const express = require('express')
 const PORT = process.env.PORT || 3000
 const app = express()
-
+const fs = require('fs');
+const pg = require('pg');
 // const __API_URL__ = https://cg-mh-booklist.herokuapp.com
+const conString = 'postgres://localhost:5432/books_app';
+
+
 
 app.get('/test', (req, res) => res.send('hello world'))
 
