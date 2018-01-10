08969e97bf37af8875ded43564dd210c38998026
cody <codyjeromegreen@gmail.com>
2018-01-09 19:41:02 -0800

    server updates

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 'use strict';
 const express = require('express')
-const PORT = process.env.PORT || 3000
-const app = express()
+const cors = require('cors')
 const fs = require('fs');
 const pg = require('pg');
+
+const app = express()
+app.use(cors())
+
+const PORT = process.env.PORT || 3000
+
+const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/books_app'
 // const __API_URL__ = https://cg-mh-booklist.herokuapp.com
 const conString = 'postgres://localhost:5432/books_app';
+
+const client = new pg.Client(DATABASE_URL)
+client.connect()
+
+app.get('/api/v1/books', (req, res) => {
+
+    client.query(`
+        SELECT book_id, title, author, image_url FROM books;
+    `).then(result => res.send(result.rows))
+        .catch(err => console.error(err))
+} )
 
 
 
