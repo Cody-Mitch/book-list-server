df63a5f8c1df4973e94c2acb73cb45ebd10337f7
cody <codyjeromegreen@gmail.com>
2018-01-13 12:39:08 -0800

    updated server file

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const fs = require('fs');
 const pg = require('pg');
 
 const app = express()
-app.use(cors())
 
 const PORT = process.env.PORT || 3000
-
 const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/books_app'
-// const __API_URL__ = https://cg-mh-booklist.herokuapp.com
-const conString = 'postgres://localhost:5432/books_app';
-
+// const conString = 'postgres://localhost:5432/books_app';
 const client = new pg.Client(DATABASE_URL)
 client.connect()
+// const __API_URL__ = https://cg-mh-booklist.herokuapp.com
+
+
+app.use(cors())
 
 app.get('/api/v1/books', (req, res) => {
 
@@ -32,8 +32,51 @@ app.get('/api/v1/books/:id', (req, res) => {
 
 })
 
+app.post('/api/v1/books', express.json(), express.urlencoded({extended:true}), (req, res) => {
+    
+    client.query(`
+        INSERT INTO books
+            (title, author, image_url, isbn, description)
+            VALUES($1, $2, $3, $4, $5);
+    `,[
+        req.body.title,
+        req.body.author,
+        req.body.image_url,
+        req.body.isbn,
+        req.body.description
+
+    ]).then(() => res.send('inserted successfully'))
+    .catch(err => console.error(err))
+})
+
+app.put('/api/v1/books/:id', express.json(), express.urlencoded({extended:true}), (req, res) => {
+    
+    client.query(`
+        UPDATE books 
+        SET title=$1, author=$2, image_url=$3, isbn=$4, description=$5
+        WHERE book_id=$6
+    `,[
+        req.body.title,
+        req.body.author,
+        req.body.image_url,
+        req.body.isbn,
+        req.body.description,
+        req.params.id
+    ]).then(result => res.send(result))
+    .catch(err => console.error(err))
+})
+
+app.delete('/api/v1/books/:id', (req, res) => {
+    client.query(`
+        DELETE FROM books WHERE book_id=${req.params.id};
+    `).then(result => res.send(result))
+    .catch(err => console.error(err))
+})
 
 
-app.get('/test', (req, res) => res.send('hello world'))
+
+
+
+// app.get('/test', (req, res) => res.send('hello world'))
 
 app.listen(PORT, () => console.log('listening on port ', PORT))
